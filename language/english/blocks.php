5e84068eedb70db3b9178726b49bb4277f16eb12
mambax7 <mambax7@gmail.com>
2015-06-02 02:34:08 -0400

    Adding English

---
+++ language/english/blocks.php
@@ -0,0 +1,14 @@
+<?php
+//  ------------------------------------------------------------------------ //
+// 本模組由 tad 製作
+// 製作日期：2015-01-22
+// $Id:$
+// ------------------------------------------------------------------------- //
+
+define('_MB_TADHONOR_LIST_OPT0', 'Show Hall of Fame marquee?');
+
+define('_MB_TADHONOR_MARQUEE_OPT0', 'Remove the Hall of Fame marquee?');
+define('_MB_TADHONOR_MARQUEE_OPT1', 'Marquee direction');
+define('_MB_TADHONOR_MARQUEE_OPT2', 'Marquee speed');
+define('_MB_TADHONOR_MARQUEE_OPT3', 'Marquee CSS Appearance Preferences');
+define('_MB_TADHONOR_MARQUEE_OPT4', 'Marquee entries CSS Appearance Preferences');
---
+++ language/english/main.php
@@ -0,0 +1,21 @@
+<?php
+//  ------------------------------------------------------------------------ //
+// 本模組由 tad 製作
+// 製作日期：2015-01-22
+// $Id:$
+// ------------------------------------------------------------------------- //
+
+
+define('_TAD_NEED_TADTOOLS', "This module needs TadTools module. You can download TadTools from <a href='http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50' target='_blank'>Tad's web</a>.");
+
+//tad_honor-list
+define('_MD_TADHONOR_HONOR_DATE', "Release date");
+define('_MD_TADHONOR_HONOR_UNIT', "Issued");
+define('_MD_TADHONOR_HONOR_COUNTER', "Viewed");
+define('_MD_TADHONOR_HONOR_CONTENT', "Details");
+define('_MD_TADHONOR_HONOR_URL', "Related Links");
+define('_MD_TADHONOR_HONOR_UID', "Author");
+
+define('_MD_TADHONOR_SHOW_HONOR_SN_FILES', "Upload");
+define('_MD_TADHONOR_HONOR_TITLE', "Title");
+define('_MD_TADHONOR_SMNAME1', "Honor Roll");
