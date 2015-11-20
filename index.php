aaaa9a44b4578c0a2e7350333341fefe3e5c6673
tnjaile <tnjaile@gmail.com>
2015-11-20 11:23:01 +0800

    修正最新消息排序
    修正最新消息排序

--- index.php
+++ index.php
@@ -13,16 +13,17 @@ include_once XOOPS_ROOT_PATH . "/header.php";
 /*-----------功能函數區--------------*/
 
 //以流水號秀出某筆tad_honor資料內容
-function show_one_tad_honor($honor_sn = "") {
+function show_one_tad_honor($honor_sn = "")
+{
     global $xoopsDB, $xoopsTpl, $isAdmin;
 
     if (empty($honor_sn)) {
         return;
     } else {
-        $honor_sn = (int)($honor_sn);
+        $honor_sn = (int) ($honor_sn);
     }
 
-    $myts =& MyTextSanitizer::getInstance();
+    $myts = &MyTextSanitizer::getInstance();
 
     $sql = "select * from `" . $xoopsDB->prefix("tad_honor") . "` where `honor_sn` = '{$honor_sn}' ";
     $result = $xoopsDB->query($sql) or redirect_header($_SERVER['PHP_SELF'], 3, mysql_error());
@@ -32,7 +33,6 @@ function show_one_tad_honor($honor_sn = "") {
     foreach ($all as $k => $v) {
         $$k = $v;
     }
-
 
     //計數器欄位值 +1
     add_tad_honor_counter($honor_sn);
@@ -50,10 +50,10 @@ function show_one_tad_honor($honor_sn = "") {
     $xoopsTpl->assign('show_honor_sn_files', $show_honor_sn_files);
 
     //過濾讀出的變數值
-    $honor_title   = $myts->htmlSpecialChars($honor_title);
-    $honor_date    = $myts->htmlSpecialChars($honor_date);
+    $honor_title = $myts->htmlSpecialChars($honor_title);
+    $honor_date = $myts->htmlSpecialChars($honor_date);
     $honor_content = $myts->displayTarea($honor_content, 1, 1, 0, 1, 0);
-    $honor_url     = $myts->htmlSpecialChars($honor_url);
+    $honor_url = $myts->htmlSpecialChars($honor_url);
 
     $xoopsTpl->assign('honor_sn', $honor_sn);
     $xoopsTpl->assign('honor_title', $honor_title);
@@ -69,7 +69,7 @@ function show_one_tad_honor($honor_sn = "") {
         redirect_header("index.php", 3, _TAD_NEED_TADTOOLS);
     }
     include_once XOOPS_ROOT_PATH . "/modules/tadtools/sweet_alert.php";
-    $sweet_alert           = new sweet_alert();
+    $sweet_alert = new sweet_alert();
     $delete_tad_honor_func = $sweet_alert->render('delete_tad_honor_func', "{$_SERVER['PHP_SELF']}?op=delete_tad_honor&honor_sn=", "honor_sn");
     $xoopsTpl->assign('delete_tad_honor_func', $delete_tad_honor_func);
 
@@ -78,7 +78,8 @@ function show_one_tad_honor($honor_sn = "") {
 }
 
 //新增tad_honor計數器
-function add_tad_honor_counter($honor_sn = '') {
+function add_tad_honor_counter($honor_sn = '')
+{
     global $xoopsDB;
     if (empty($honor_sn)) {
         return;
@@ -88,7 +89,8 @@ function add_tad_honor_counter($honor_sn = '') {
 }
 
 //刪除tad_honor某筆資料資料
-function delete_tad_honor($honor_sn = "") {
+function delete_tad_honor($honor_sn = "")
+{
     global $xoopsDB, $isAdmin;
     if (empty($honor_sn)) {
         return;
@@ -103,50 +105,50 @@ function delete_tad_honor($honor_sn = "") {
 }
 
 //列出所有tad_honor資料
-function list_tad_honor() {
+function list_tad_honor()
+{
     global $xoopsDB, $xoopsTpl, $isAdmin;
 
-    $myts =& MyTextSanitizer::getInstance();
+    $myts = &MyTextSanitizer::getInstance();
 
     include_once XOOPS_ROOT_PATH . "/modules/tadtools/TadUpFiles.php";
     $TadUpFiles = new TadUpFiles("tad_honor");
 
-    $sql = "select * from `" . $xoopsDB->prefix("tad_honor") . "` ";
+    $sql = "select * from `" . $xoopsDB->prefix("tad_honor") . "` order by `honor_date` desc ";
 
     //getPageBar($原sql語法, 每頁顯示幾筆資料, 最多顯示幾個頁數選項);
     $PageBar = getPageBar($sql, 20, 10, null, null, $bootstrap);
-    $bar     = $PageBar['bar'];
-    $sql     = $PageBar['sql'];
-    $total   = $PageBar['total'];
+    $bar = $PageBar['bar'];
+    $sql = $PageBar['sql'];
+    $total = $PageBar['total'];
 
     $result = $xoopsDB->query($sql) or redirect_header($_SERVER['PHP_SELF'], 3, mysql_error());
 
     $all_content = "";
-    $i           = 0;
+    $i = 0;
     while ($all = $xoopsDB->fetchArray($result)) {
         //以下會產生這些變數： $honor_sn , $honor_title , $honor_date , $honor_unit , $honor_counter , $honor_content , $honor_url , $honor_uid
         foreach ($all as $k => $v) {
             $$k = $v;
         }
 
-
         //過濾讀出的變數值
-        $honor_title   = $myts->htmlSpecialChars($honor_title);
-        $honor_date    = $myts->htmlSpecialChars($honor_date);
+        $honor_title = $myts->htmlSpecialChars($honor_title);
+        $honor_date = $myts->htmlSpecialChars($honor_date);
         $honor_content = $myts->displayTarea($honor_content, 1, 1, 0, 1, 0);
-        $honor_url     = $myts->htmlSpecialChars($honor_url);
+        $honor_url = $myts->htmlSpecialChars($honor_url);
 
-        $all_content[$i]['honor_sn']         = $honor_sn;
+        $all_content[$i]['honor_sn'] = $honor_sn;
         $all_content[$i]['honor_title_link'] = "<a href='{$_SERVER['PHP_SELF']}?honor_sn={$honor_sn}'>{$honor_title}</a>";
-        $all_content[$i]['honor_title']      = $honor_title;
-        $all_content[$i]['honor_date']       = $honor_date;
-        $all_content[$i]['honor_unit']       = $honor_unit;
-        $all_content[$i]['honor_counter']    = $honor_counter;
-        $all_content[$i]['honor_content']    = $honor_content;
-        $all_content[$i]['honor_url']        = $honor_url;
-        $all_content[$i]['honor_uid']        = $uid_name;
+        $all_content[$i]['honor_title'] = $honor_title;
+        $all_content[$i]['honor_date'] = $honor_date;
+        $all_content[$i]['honor_unit'] = $honor_unit;
+        $all_content[$i]['honor_counter'] = $honor_counter;
+        $all_content[$i]['honor_content'] = $honor_content;
+        $all_content[$i]['honor_url'] = $honor_url;
+        $all_content[$i]['honor_uid'] = $uid_name;
         $TadUpFiles->set_col("honor_sn", $honor_sn);
-        $show_files                   = $TadUpFiles->show_files('up_honor_sn', true, 'small', true, false, null, null, false);
+        $show_files = $TadUpFiles->show_files('up_honor_sn', true, 'small', true, false, null, null, false);
         $all_content[$i]['list_file'] = $show_files;
         $i++;
     }
@@ -163,24 +165,22 @@ function list_tad_honor() {
         redirect_header("index.php", 3, _TAD_NEED_TADTOOLS);
     }
     include_once XOOPS_ROOT_PATH . "/modules/tadtools/sweet_alert.php";
-    $sweet_alert           = new sweet_alert();
+    $sweet_alert = new sweet_alert();
     $delete_tad_honor_func = $sweet_alert->render('delete_tad_honor_func', "{$_SERVER['PHP_SELF']}?op=delete_tad_honor&honor_sn=", "honor_sn");
     $xoopsTpl->assign('delete_tad_honor_func', $delete_tad_honor_func);
 }
 
 /*-----------執行動作判斷區----------*/
-$op       = empty($_REQUEST['op']) ? "" : $_REQUEST['op'];
-$honor_sn = empty($_REQUEST['honor_sn']) ? "" : (int)($_REQUEST['honor_sn']);
-$files_sn = empty($_REQUEST['files_sn']) ? "" : (int)($_REQUEST['files_sn']);
+$op = empty($_REQUEST['op']) ? "" : $_REQUEST['op'];
+$honor_sn = empty($_REQUEST['honor_sn']) ? "" : (int) ($_REQUEST['honor_sn']);
+$files_sn = empty($_REQUEST['files_sn']) ? "" : (int) ($_REQUEST['files_sn']);
 
 switch ($op) {
     /*---判斷動作請貼在下方---*/
 
-
     case "tad_honor_form":
         tad_honor_form($honor_sn);
         break;
-
 
     case "delete_tad_honor":
         delete_tad_honor($honor_sn);
@@ -193,7 +193,6 @@ switch ($op) {
         exit;
         break;
 
-
     default:
         if (empty($honor_sn)) {
             list_tad_honor();
@@ -203,7 +202,7 @@ switch ($op) {
         }
         break;
 
-    /*---判斷動作請貼在上方---*/
+        /*---判斷動作請貼在上方---*/
 }
 
 /*-----------秀出結果區--------------*/
