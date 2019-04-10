765c09cbfc645bf93646bffeb399722ff3877104
mambax7 <mambax7@gmail.com>
2019-04-10 01:31:38 -0400

    function name conflict
    Signed-off-by: mambax7 <mambax7@gmail.com>

--- include/onInstall.php
+++ include/onInstall.php
@@ -3,16 +3,16 @@
 function xoops_module_install_tad_honor(&$module)
 {
 
-    mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_honor");
-    mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_honor/file");
-    mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_honor/image");
-    mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_honor/image/.thumbs");
+    tad_honor_mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_honor");
+    tad_honor_mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_honor/file");
+    tad_honor_mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_honor/image");
+    tad_honor_mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_honor/image/.thumbs");
 
     return true;
 }
 
 //建立目錄
-function mk_dir($dir = "")
+function tad_honor_mk_dir($dir = "")
 {
     //若無目錄名稱秀出警告訊息
     if (empty($dir)) {
