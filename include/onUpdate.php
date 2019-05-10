fd83bd6c484666ea90f063a3deef654251c32b0f
Tad <tad0616@gmail.com>
2019-05-10 16:35:42 +0800

    更新說明

--- include/onUpdate.php
+++ include/onUpdate.php
@@ -2,7 +2,12 @@
 
 use XoopsModules\Tadtools\Utility;
 use XoopsModules\Tad_honor\Update;
-
+if (!class_exists('XoopsModules\Tadtools\Utility')) {
+    require XOOPS_ROOT_PATH . '/modules/tadtools/preloads/autoloader.php';
+}
+if (!class_exists('XoopsModules\Tad_honor\Update')) {
+    include dirname(__DIR__) . '/preloads/autoloader.php';
+}
 function xoops_module_update_tad_honor(&$module, $old_version)
 {
     global $xoopsDB;
---
+++ templates/blocks/tad_honor_marquee.tpl
@@ -0,0 +1,2 @@
+<{assign var=this_file value=$smarty.template|basename|replace:'db:':''}>
+<{includeq file="$xoops_rootpath/modules/tad_honor/templates/blocks/b4.tpl"}>
