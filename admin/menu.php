fb68194fb0166bcbc9c0243dcbdf42b1519e6bae
tad0616 <tad0616@gmail.com>
2024-11-12 16:21:18 +0800

    簡化後台選單陣列，修改釋出日期

--- admin/menu.php
+++ admin/menu.php
@@ -1,32 +1,23 @@
 <?php
-$adminmenu = [];
-$i = 1;
-
-$adminmenu[$i]['title'] = _MI_TAD_ADMIN_HOME;
-$adminmenu[$i]['link'] = 'admin/index.php';
-$adminmenu[$i]['desc'] = _MI_TAD_ADMIN_HOME_DESC;
-$adminmenu[$i]['icon'] = 'images/admin/home.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TADHONOR_ADMENU2;
-$adminmenu[$i]['link'] = 'admin/main.php';
-$adminmenu[$i]['desc'] = _MI_TADHONOR_ADMENU2_DESC;
-$adminmenu[$i]['icon'] = 'images/admin/badge.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TADHONOR_ADMENU3;
-$adminmenu[$i]['link'] = 'admin/import.php';
-$adminmenu[$i]['desc'] = _MI_TADHONOR_ADMENU3_DESC;
-$adminmenu[$i]['icon'] = 'images/admin/synchronized.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TADHONOR_ADMENU4;
-$adminmenu[$i]['link'] = 'admin/import_tadnews.php';
-$adminmenu[$i]['desc'] = _MI_TADHONOR_ADMENU4_DESC;
-$adminmenu[$i]['icon'] = 'images/admin/synchronized.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TAD_ADMIN_ABOUT;
-$adminmenu[$i]['link'] = 'admin/about.php';
-$adminmenu[$i]['desc'] = _MI_TAD_ADMIN_ABOUT_DESC;
-$adminmenu[$i]['icon'] = 'images/admin/about.png';
+$adminmenu = [
+    [
+        'title' => _MI_TADHONOR_ADMENU2,
+        'link' => 'admin/main.php',
+        'icon' => 'images/admin/badge.png',
+    ],
+    [
+        'title' => _MI_TADHONOR_ADMENU3,
+        'link' => 'admin/import.php',
+        'icon' => 'images/admin/synchronized.png',
+    ],
+    [
+        'title' => _MI_TADHONOR_ADMENU4,
+        'link' => 'admin/import_tadnews.php',
+        'icon' => 'images/admin/synchronized.png',
+    ],
+    [
+        'title' => _MI_TAD_ADMIN_ABOUT,
+        'link' => 'admin/about.php',
+        'icon' => 'images/admin/about.png',
+    ],
+];
